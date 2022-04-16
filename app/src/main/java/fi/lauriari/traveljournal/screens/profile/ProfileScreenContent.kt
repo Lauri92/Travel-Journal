ee7812c5d4e5f1f8bf1bba956ddb0149644b0950
Lauri92 <55086903+Lauri92@users.noreply.github.com>
2022-04-16 20:33:10 +0300

    Implemented logout which navigates back to login screen and removes token and username from shared prefs

--- app/src/main/java/fi/lauriari/traveljournal/screens/profile/ProfileScreenContent.kt
+++ app/src/main/java/fi/lauriari/traveljournal/screens/profile/ProfileScreenContent.kt
@@ -1,27 +1,49 @@
 package fi.lauriari.traveljournal.screens.profile
 
-import android.util.Log
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.OutlinedButton
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 import fi.lauriari.traveljournal.ui.theme.backGroundBlue
 import fi.lauriari.traveljournal.util.User
 
 @Composable
-fun ProfileScreenContent(navigateToLoginScreen: () -> Unit) {
+fun ProfileScreenContent(
+    navigateToLoginScreen: () -> Unit
+) {
     Column(
         modifier = Modifier
             .background(color = backGroundBlue)
             .fillMaxSize(),
     ) {
-        Column() {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+        ) {
             val context = LocalContext.current
-            Text(text = "Logout ${User.getUsername(context)}", color = Color.Black)
+            OutlinedButton(
+                modifier = Modifier
+                    .padding(end = 10.dp, top = 10.dp)
+                    .align(Alignment.TopEnd),
+                shape = CircleShape,
+                onClick = {
+                    User.removeToken(context)
+                    User.removeUsername(context)
+                    navigateToLoginScreen()
+                }
+            ) {
+                Text(
+                    text = "Logout",
+                    color = Color.Black
+                )
+            }
         }
     }
 }
