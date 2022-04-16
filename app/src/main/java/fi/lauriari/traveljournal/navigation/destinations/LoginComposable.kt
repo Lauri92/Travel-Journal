24d0f516e1b97216de711df8af14f18bea14d48c
Lauri92 <55086903+Lauri92@users.noreply.github.com>
2022-04-16 19:01:15 +0300

    Automatic navigation to profile screen if a token exists, a non null token is always assumed to be a valid one

--- app/src/main/java/fi/lauriari/traveljournal/navigation/destinations/LoginComposable.kt
+++ app/src/main/java/fi/lauriari/traveljournal/navigation/destinations/LoginComposable.kt
@@ -1,10 +1,14 @@
 package fi.lauriari.traveljournal.navigation.destinations
 
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.platform.LocalContext
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
 import fi.lauriari.traveljournal.screens.login.LoginScreen
 import fi.lauriari.traveljournal.util.Constants.LOGIN_SCREEN
+import fi.lauriari.traveljournal.util.User
 import fi.lauriari.traveljournal.viewmodels.LoginViewModel
+import kotlinx.coroutines.launch
 
 fun NavGraphBuilder.loginComposable(
     loginViewModel: LoginViewModel,
@@ -13,9 +17,17 @@ fun NavGraphBuilder.loginComposable(
     composable(
         route = LOGIN_SCREEN
     ) { navBackStackEntry ->
-        LoginScreen(
-            loginViewModel = loginViewModel,
-            navigateToUserScreen = navigateToUserScreen,
-        )
+        val scope = rememberCoroutineScope()
+
+        if (User.getToken(LocalContext.current) == null) {
+            LoginScreen(
+                loginViewModel = loginViewModel,
+                navigateToUserScreen = navigateToUserScreen,
+            )
+        } else {
+            scope.launch {
+                navigateToUserScreen()
+            }
+        }
     }
 }
